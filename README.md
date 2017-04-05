5e381f3678d090c32e39431e0d3260c4d2f982de
hachisanju <thomas@meltchannel.com>
2017-04-05 14:05:31 -0400

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# TP-Link Smart Bulb Controller for Python
+
+This is a controller to turn on and off TP-Link Smart Bulbs, specifically the LB-120. It's based on Lubomir Stroetmann's work [here](https://github.com/softScheck/tplink-smartplug). 
+
+If you're interested in the full article, that's also here: [Reverse Engineering the TP-Link HS110](https://www.softscheck.com/en/reverse-engineering-tp-link-hs110/)
+
+I won't get elaborate with the usage. It's the same as the tplink-smartplug.py program.
+Invoke like this:
+`./tplink-smarthome.py -t <ip> [-c <cmd> || -j <json>]`
+
+The available commands are on, off and info. Anything else you'll have to send as JSON. If you want more in depth commands just go visit Lubomir's github, seriously. It's a good resource.
---
+++ tplink-smartbulb.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# 
+#  
+# This code is HEAVILY based on tplink_smartplug.py by 
+# Lubomir Stroetmann and is Copyrighted 2016 softScheck GmbH
+# The original source code is under the Apache License:
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# It can be found here:
+#
+# https://github.com/softScheck/tplink-smartplug
+#
+#
+# If you wanna take my modifications, alone (?) without the
+# original code, you can consider it WTFPL
+#
+#											-hachisanju
+#
+
+import socket
+import argparse
+
+version = 0.1
+
+# Check if IP is valid
+def validIP(ip):
+	try:
+		socket.inet_pton(socket.AF_INET, ip)
+	except socket.error:
+		parser.error("Invalid IP Address.")
+	return ip 
+
+commands = {'info'     : '{"system":{"get_sysinfo":{}}}',
+			'on'       : '{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":1}}}',
+			'off'      : '{"smartlife.iot.smartbulb.lightingservice":{"transition_light_state":{"on_off":0}}}'
+}
+
+# Encryption and Decryption of TP-Link Smart Home Protocol
+# XOR Autokey Cipher with starting key = 171
+def encrypt(string):
+	key = 171
+	result = ""
+	for i in string: 
+		a = key ^ ord(i)
+		key = a
+		result += chr(a)
+	return result
+
+def decrypt(string):
+	key = 171 
+	result = ""
+	for i in string: 
+		a = key ^ ord(i)
+		key = ord(i) 
+		result += chr(a)
+	return result
+
+# Parse commandline arguments
+parser = argparse.ArgumentParser(description="TP-Link Wi-Fi Smart Plug Client v" + str(version))
+parser.add_argument("-t", "--target", metavar="<ip>", required=True, help="Target IP Address", type=validIP)
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument("-c", "--command", metavar="<command>", help="Preset command to send. Choices are: "+", ".join(commands), choices=commands) 
+group.add_argument("-j", "--json", metavar="<JSON string>", help="Full JSON string of command to send")
+args = parser.parse_args()
+
+# Set target IP, port and command to send
+ip = args.target
+port = 9999
+if args.command is None:
+	cmd = args.json
+else:
+	cmd = commands[args.command]
+
+# Send command and receive reply 
+try:
+	
+	sock_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	sock_udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        sock_udp.sendto(encrypt(cmd), (ip, port))
+	print "Sent:     ", cmd
+        data = sock_udp.recv(1024)
+
+	print "Sent:     ", cmd
+        print "Received: ", decrypt(data[4:])
+
+
+except socket.error:
+	quit("Cound not connect to host " + ip + ":" + str(port))
